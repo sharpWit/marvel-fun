fd1bd74c72511ee37c39c2f6d02344edb0b4e7a7
sharpWit <khosravi.webmaster@gmail.com>
2024-08-18 15:32:39 +0330

    fix(comics): fix some types

--- src/app/comics/page.tsx
+++ src/app/comics/page.tsx
@@ -1,26 +1,32 @@
 import { cache } from "react";
 import { NextPage } from "next";
 import { PAGE_SIZE } from "@/lib/constants";
+import { IComicsInfo } from "@/types/comics";
+import { IMarvelRes } from "@/types/response";
 import { getAllComics } from "@/services/endpoints";
 import AxiosAdapter from "@/services/fetch-in-server";
 import ComicsCard from "@/components/comic-card/ComicsCard";
 import { apiKeyParam, hashParam, tsParam } from "@/lib/urlParams";
 
-const { url: charURL, method, data } = getAllComics();
+const { url: comicURL, method, data } = getAllComics();
 
-const getComics = cache(async (params?: any) => {
-  const offset = params ?? 0 * PAGE_SIZE;
-  const res = await AxiosAdapter(
-    {
-      url: `${charURL}?${apiKeyParam}&${tsParam}&${hashParam}&offset=${offset}&limit=${PAGE_SIZE}`,
-      method,
-      data,
-    },
-    undefined,
-    false
-  );
-  return res;
-});
+const getComics = cache(
+  async (params?: number): Promise<IMarvelRes<IComicsInfo>> => {
+    const offset =
+      (typeof params !== "number" || isNaN(params) ? 0 : params) * PAGE_SIZE;
+
+    const res: IMarvelRes<IComicsInfo> = await AxiosAdapter(
+      {
+        url: `${comicURL}?${apiKeyParam}&${tsParam}&${hashParam}&offset=${offset}&limit=${PAGE_SIZE}`,
+        method,
+        data,
+      },
+      undefined,
+      false
+    );
+    return res;
+  }
+);
 
 interface Props {
   searchParams: any;
