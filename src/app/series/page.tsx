c83c77b24eb79058f7cfd0b41d92221db21c775c
sharpWit <khosravi.webmaster@gmail.com>
2024-08-18 17:31:39 +0330

    refactor(series): update some prev code

--- src/app/series/page.tsx
+++ src/app/series/page.tsx
@@ -1,13 +1,43 @@
-import dynamic from "next/dynamic";
+import { cache } from "react";
+import { NextPage } from "next";
+import { PAGE_SIZE } from "@/lib/constants";
+import { ISeriesInfo } from "@/types/series";
+import { IMarvelRes } from "@/types/response";
+import { getAllChars } from "@/services/endpoints";
+import AxiosAdapter from "@/services/fetch-in-server";
+import SeriesCard from "@/components/series-card/SeriesCard";
+import { apiKeyParam, hashParam, tsParam } from "@/lib/urlParams";
 
-const SeriesPage = () => {
-  const SeriesCard = dynamic(
-    () => import("@/components/series-card/SeriesCard"),
-    {
-      ssr: false,
-    }
-  );
+const { url: serieURL, method, data } = getAllChars();
 
-  return <SeriesCard />;
+const getSeries = cache(
+  async (params?: number): Promise<IMarvelRes<ISeriesInfo>> => {
+    const offset =
+      (typeof params !== "number" || isNaN(params) ? 0 : params) * PAGE_SIZE;
+
+    const res: IMarvelRes<ISeriesInfo> = await AxiosAdapter(
+      {
+        url: `${serieURL}?${apiKeyParam}&${tsParam}&${hashParam}&offset=${offset}&limit=${PAGE_SIZE}`,
+        method,
+        data,
+      },
+      undefined,
+      false
+    );
+    // console.log("RES: ", res);
+
+    return res;
+  }
+);
+
+interface Props {
+  searchParams: any;
+}
+
+const SeriesPage: NextPage<Props> = async ({ searchParams }) => {
+  const { page } = searchParams ?? {};
+  const series = await getSeries(Number(page));
+
+  return <SeriesCard marvelSeries={series} />;
 };
 export default SeriesPage;
